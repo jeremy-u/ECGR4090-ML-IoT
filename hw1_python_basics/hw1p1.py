304a29f1b267bcd9bedcf0448ac718b11811cbc9
Jeremy Holleman <jeremy@syntiant.com>
2021-01-26 12:39:57 -0500

    v0 starter code for hw1

---
+++ hw1_python_basics/hw1p1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+list_of_names = ['Roger', 'Mary', 'Luisa', 'Elvis']
+list_of_ages  = [23, 24, 19, 86]
+list_of_heights_cm = [175, 162, 178, 182]
+
+for name in list_of_names:
+  print("The name {:} is {:} letters long".format(name, len(name)))
+  
+
