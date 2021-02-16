83f698ef0d6c9db3b5cad21b9489a8e23fe50bbc
Jeremy Holleman <jeremy@syntiant.com>
2021-02-16 13:13:59 -0500

    added hw2 materials

---
+++ hw2/hw2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+from tensorflow.keras import Input, layers
+
+print(tf.__version__)
+
